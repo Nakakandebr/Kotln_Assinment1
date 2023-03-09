f891f114004184bd04b1726c4a294f33ccc3b623
Nakakandebr <bridgetnakakandea@gmail.com>
2023-03-09 09:01:04 +0300

    commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,6 +4,8 @@ var now=Calculator("Addition","Subtraction","division")
     now.subtraction(4,5)
     println(now.subtraction(3,6))
     println()
+    println(fu(36,7,8))
+
 
 }
 //Create a calculator class that is capable of the following functions:
@@ -16,7 +18,14 @@ class Calculator(var add:String,var subt:String,var divide:String){
     fun subtraction(Y:Int,X:Int){
 
     }
-    fun add()
 
+
+
+}
+//Write and invoke one function that takes in an array of integers and returns
+//these 3 values: smallest, largest and average of all the elements
+fun fu(num1:Int,num2:Int,num3:Int):Int{
+    var numarray= arrayOf(num1,num2,num3)
+    return(numarray.max())
 
 }
