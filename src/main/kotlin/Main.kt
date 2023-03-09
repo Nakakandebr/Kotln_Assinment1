325a97eb2d499c205a457fbc0a5f137fa8e2aaff
Nakakandebr <bridgetnakakandea@gmail.com>
2023-03-09 08:52:40 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,22 @@
+
+fun main(){
+var now=Calculator("Addition","Subtraction","division")
+    now.subtraction(4,5)
+    println(now.subtraction(3,6))
+    println()
+
+}
+//Create a calculator class that is capable of the following functions:
+//(i) Addition of any 2 numbers
+//(ii) Subtraction between any 2 numbers
+//(iii) Accurate division between any 2 numbers
+//(iv) Multiplication of any 2 numbers
+//Instantiate your calculator object and invoke all its functions
+class Calculator(var add:String,var subt:String,var divide:String){
+    fun subtraction(Y:Int,X:Int){
+
+    }
+    fun add()
+
+
+}
